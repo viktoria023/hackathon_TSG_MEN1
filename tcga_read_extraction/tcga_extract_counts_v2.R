f3cdaa28c652ee732ce24b0afe5a2d44a109cd0f
viktoria023 <62943294+viktoria023@users.noreply.github.com>
2021-12-16 16:17:15 +0000

    merged two extraction files to one working copy

--- tcga_read_extraction/tcga_extract_counts_v2.R
+++ tcga_read_extraction/tcga_extract_counts_v2.R
@@ -1,6 +1,7 @@
 library(GenomicDataCommons)
 library(dplyr)
-setwd("~/Desktop/Hackhathon")
+path<- strsplit(getwd(),'/')[[1]]
+if (isFALSE(path[length(path)]=="hackathon_TSG_MEN1")){stop("Set working directory to hackathon_TSG_MEN1")} 
 
 #load curated gene list
 DDR_epi_genes_names <- readRDS("DDR_epi_genes_names.rds")
@@ -15,14 +16,14 @@ DDR_epi_genes_names <- rbind(DDR_epi_genes_names, Men1)
 file_names <- readRDS("cancer_files_lists.rds")
 cancers <- names(file_names)
 
-setwd("~/Desktop/Hackhathon/cancer-data-all-files")
+setwd("./cancer-data-all-files")
 
 #k==chooses cancer type from list. 1 is already done, and I will continue with 2-4
-k=1
+k=13
 list <- file_names[[k]]
 
 #extract counts for genes of interest from datasets
-counts_genes <- ensmbl <- DDR_epi_genes_names$Gene.stable.ID
+counts_genes <- DDR_epi_genes_names$Gene.stable.ID
 
 #upload all files and make them as dataframe
 for (i in 1:length(list)){
@@ -32,32 +33,30 @@ for (i in 1:length(list)){
   if (i == 1){
     counts <- get(paste("raw_counts",i, sep = "_"))
     next
-    }
-  counts <- merge(counts, get(paste("raw_counts",i, sep = "_")), by = "feature", all = TRUE )
   }
+  counts <- merge(counts, get(paste("raw_counts",i, sep = "_")), by = "feature", all = TRUE )
+  print(i)
+}
+
+backup <- counts
+
 colnames(counts) <- c("feature", 1:length(list))
 
 counts$feature <- gsub("\\..*", "", counts$feature)
 
-counts <- counts %>% 
-  filter(feature %in% counts_genes)
-
-# This is the dataframe above, it is called "counts"
-# now we need to make it formatted for DESeq2
+counts <- counts %>% filter(feature %in% counts_genes)
 
 #reformat counts matrix to make applicable to DESeq2
-rownames(counts_genes) <- counts_genes[,1]
-counts_genes <- counts_genes[,-1]
-counts_genes <- as.matrix(counts_genes)
-colnames(counts_genes) <- NULL
+rownames(counts) <- counts[,1]
+counts <- counts[,-1]
 
-counts_genes <- as.data.frame(counts_genes)
-counts_genes_tmp <- apply(as.matrix.noquote(counts_genes),2,as.numeric)
-rownames(counts_genes_tmp) <- rownames(counts_genes)
-counts_genes <- counts_genes_tmp
-counts_genes <- as.data.frame(counts_genes)
+counts <- as.data.frame(counts)
+counts_tmp <- apply(as.matrix.noquote(counts),2,as.numeric)
+rownames(counts_tmp) <- rownames(counts)
+counts <- counts_tmp
+counts <- as.data.frame(counts)
 
 #save as a list of dataframes
-cancer_data <- list(counts_genes)
-savefile<-paste0(paste0(cancers[k]), "_raw_data.rds")
+cancer_data <- list(counts)
+savefile <- paste0(paste0(cancers[k]), "_raw_data.rds")
 saveRDS(cancer_data, file = savefile)
